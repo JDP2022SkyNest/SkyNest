956f5a50d1e89967cf1f12e490a1c708fd616263
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-05-17 11:12:33 +0200

    spring init

---
+++ backend/src/main/java/com/htecgroup/SkyNest/SkyNestApplication.java
@@ -0,0 +1,13 @@
+package com.htecgroup.SkyNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyNestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/htecgroup/SkyNest/SkyNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htecgroup.SkyNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
