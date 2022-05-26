09feff2d8136a0187fb374f6d4c77d059d0c15b3
casualme2 <miljan.pantovic@htecgroup.com>
2022-05-26 14:12:22 +0200

    Removed unnecessary duplicate code.

--- frontend/src/Components/Loader/Loader.jsx
+++ frontend/src/Components/Loader/Loader.jsx
@@ -3,9 +3,7 @@ import "./Loader.css";
 
 const Loader = () => (
 	<div className="container-fluid d-flex flex-column justify-content-center align-items-center vh-100">
-		<div className="spinner-border text-primary loader-circle-size" role="status">
-			<span className="sr-only">Loading...</span>
-		</div>
+		<div className="spinner-border text-primary loader-circle-size" role="status"></div>
 		<br />
 		<div className="text-muted loader-text-size">Loading your data...</div>
 	</div>
