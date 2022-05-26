1e51d226f3fb568ede80f1d788f1175eebf28509
casualme2 <miljan.pantovic@htecgroup.com>
2022-05-26 12:03:16 +0200

    PR Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Loader from "./Components/Loader/Loader";
 
-function App() {
+const App = () => {
 	return <Loader />;
-}
+};
 
 export default App;
--- frontend/src/Components/Loader/Loader.jsx
+++ frontend/src/Components/Loader/Loader.jsx
@@ -1,24 +1,14 @@
 import React from "react";
-
-// Our Fake Data - Will be changed when we get our endpoint and data from there
+import "./Loader.css";
 
 const Loader = () => {
 	return (
-		// Loader animation with Text - Can be changed whenever, however
 		<div className="container-fluid d-flex flex-column justify-content-center align-items-center vh-100">
-			{/* Main Loader Style */}
-			<div className="spinner-border text-primary" style={{ width: "5rem", height: "5rem" }} role="status">
+			<div className="spinner-border text-primary loader-circle-size" role="status">
 				<span className="sr-only">Loading...</span>
 			</div>
-
-			{/* This is the optional Loader that we may use. */}
-			{/* <div className="spinner-grow" style={{ width: "5rem", height: "5rem" }} role="status">
-					<span className="sr-only">Loading...</span>
-				</div> */}
 			<br />
-			<div className="text-muted" style={{ fontSize: "20px" }}>
-				Loading your data...
-			</div>
+			<div className="text-muted loader-text-size">Loading your data...</div>
 		</div>
 	);
 };
