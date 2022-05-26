f9cef60f1a69637a1cb7874fddd4af0d91f20414
casualme2 <miljan.pantovic@htecgroup.com>
2022-05-26 15:18:27 +0200

    Implemented Axios with MockAPI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import HomePage from "./Components/HomePage/HomePage";
 import Login from "./Components/Login/Login";
 
-const App = () => <Login />;
+const App = () => {
+	const [accessToken, setAccessToken] = useState(localStorage.accessToken);
+
+	if (!accessToken) {
+		return <Login setAccessToken={setAccessToken} />;
+	}
+	return <HomePage />;
+};
 
 export default App;
--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Login.css";
 import logoImage from "./assets/logoblackandwhite.svg";
+import AxiosInstance from "../axios/AxiosInstance";
 
-const Login = () => {
+const Login = ({ setAccessToken }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMsg, setErrorMsg] = useState("");
+
+	const emailRef = useRef();
+
+	const getUserToken = async () => {
+		AxiosInstance.post("/login", {
+			email: email,
+			password: password,
+		})
+			.then(({ data }) => {
+				setAccessToken(data.accessToken);
+				localStorage.setItem("accessToken", data.accessToken);
+			})
+			.catch(({ response }) => {
+				if (response.status === 400) {
+					setErrorMsg("Incorrect username or password");
+				} else if (response.status === 0) {
+					setErrorMsg("Server Timeout");
+				}
+				emailRef.current.focus();
+			});
+	};
+
+	useEffect(() => {
+		emailRef.current.focus();
+	}, []);
+
+	useEffect(() => {
+		setErrorMsg("");
+	}, [email, password]);
+
+	const onFormSubmit = (e) => {
+		e.preventDefault();
+		getUserToken();
+
+		setEmail("");
+		setPassword("");
+	};
+
 	return (
 		<section className="vh-100 container-fluid page-background-color">
 			<div className="container py-5 h-100">
@@ -15,7 +57,7 @@ const Login = () => {
 								</div>
 								<div className="col-md-6 col-lg-7 d-flex align-items-center">
 									<div className="card-body p-4 p-lg-5 text-black">
-										<form>
+										<form onSubmit={onFormSubmit}>
 											<div className="d-flex align-items-center mb-3 pb-1">
 												<span className="d-flex justify-content-center m-auto">
 													<img src={logoImage} alt="logo" className="little-image-logo d-md-none d-xs-flex" />{" "}
@@ -23,22 +65,36 @@ const Login = () => {
 												</span>
 											</div>
 											<h5 className="mb-3 pb-3 text-center text-secondary">Sign into your account</h5>
+											<p className={errorMsg ? "alert alert-danger text-danger text-center" : "d-none"}>{errorMsg}</p>
 											<div className="form-outline mb-4">
-												<input type="email" id="form2Example17" className="form-control form-control-lg" />
-												<label className="form-label" for="form2Example17">
+												<input
+													type="email"
+													onChange={(e) => setEmail(e.target.value)}
+													value={email}
+													ref={emailRef}
+													id="emailInput"
+													className="form-control form-control-lg"
+													required
+												/>
+												<label className="form-label" htmlFor="emailInput">
 													Email address
 												</label>
 											</div>
 											<div className="form-outline mb-4">
-												<input type="password" id="form2Example27" className="form-control form-control-lg" />
-												<label className="form-label" for="form2Example27">
+												<input
+													type="password"
+													onChange={(e) => setPassword(e.target.value)}
+													value={password}
+													id="passwordInput"
+													className="form-control form-control-lg"
+													required
+												/>
+												<label className="form-label" htmlFor="passwordInput">
 													Password
 												</label>
 											</div>
 											<div className="pt-1 mb-4">
-												<button className="btn btn-dark btn-lg btn-block" type="button">
-													Login
-												</button>
+												<button className="btn btn-dark btn-lg btn-block">Login</button>
 											</div>
 											<div className="mt-2 text-center">
 												<p className="">Don't have an account? </p>
