6503c04b3efe7caac414d700926fd566f3f03291
Darko Veljanovski <darko.veljanoski@htecgroup.com>
2022-05-30 15:31:46 +0200

    Merge issues - refactoring

--- backend/src/main/java/com/htecgroup/SkyNest/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/htecgroup/SkyNest/service/impl/UserServiceImpl.java
@@ -19,54 +19,53 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
-  @Autowired private UserRepository userRepository;
-  @Autowired private RoleRepository roleRepository;
-  @Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
-  @Autowired private ModelMapper modelMapper;
+    @Autowired private UserRepository userRepository;
+    @Autowired private RoleRepository roleRepository;
+    @Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired private ModelMapper modelMapper;
 
-  @Override
-  public UserDto registerUser(UserDto userDto) {
+    @Override
+    public UserDto registerUser(UserDto userDto) {
 
 
-    if (userRepository.existsByEmail(userDto.getEmail())) {
-      throw new UserException(UserExceptionType.EMAIL_ALREADY_IN_USE);
-      }
+        if (userRepository.existsByEmail(userDto.getEmail())) {
+            throw new UserException(UserExceptionType.EMAIL_ALREADY_IN_USE);
+        }
 
-    if (userRepository.findUserByEmail(userDto.getEmail()) != null) {
-       throw new RuntimeException("Email already in use");
+        if (userRepository.findUserByEmail(userDto.getEmail()) != null) {
+            throw new RuntimeException("Email already in use");
 
+        }
+
+        RoleEntity roleEntity =
+                roleRepository
+                        .findByName(RoleEntity.ROLE_WORKER)
+                        .orElseThrow(
+                                () ->
+                                        new UserException(
+                                                "Role " + RoleEntity.ROLE_WORKER + " not found.",
+                                                HttpStatus.INTERNAL_SERVER_ERROR));
+        userDto.setRole(modelMapper.map(roleEntity, RoleDto.class));
+
+        userDto.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+        userDto.setVerified(false);
+        userDto.setEnabled(false);
+
+        UserEntity userEntity = userRepository.save(modelMapper.map(userDto, UserEntity.class));
+
+        return modelMapper.map(userEntity, UserDto.class);
     }
 
-    RoleEntity roleEntity =
-            roleRepository
-                    .findByName(RoleEntity.ROLE_WORKER)
-                    .orElseThrow(
-                            () ->
-                                    new UserException(
-                                            "Role " + RoleEntity.ROLE_WORKER + " not found.",
-                                            HttpStatus.INTERNAL_SERVER_ERROR));
-    userDto.setRole(modelMapper.map(roleEntity, RoleDto.class));
+    @Override
+    public UserDto findUserByEmail(String email) {
 
-    userDto.setEncryptedPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
-    userDto.setVerified(false);
-    userDto.setEnabled(false);
+        UserEntity userEntity =
+                userRepository
+                        .findUserByEmail(email)
+                        .orElseThrow(
+                                () -> new UsernameNotFoundException("could not find user with email: " + email));
 
-    UserEntity userEntity = userRepository.save(modelMapper.map(userDto, UserEntity.class));
-
-    return modelMapper.map(userEntity, UserDto.class);
-  }
-
-  @Override
-  public UserDto findUserByEmail(String email) {
-
-    UserEntity userEntity =
-            userRepository
-                    .findUserByEmail(email)
-                    .orElseThrow(
-                            () -> new UsernameNotFoundException("could not find user with email: " + email));
-
-    return modelMapper.map(userEntity, UserDto.class);
-  }
+        return modelMapper.map(userEntity, UserDto.class);
+    }
 
 }
-
