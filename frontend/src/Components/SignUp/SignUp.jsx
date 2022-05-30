0f852db188e835e53c383ccca673a27ff0311c65
casualme2 <miljan.pantovic@htecgroup.com>
2022-05-30 12:48:43 +0200

    Building and Linking SignUp page

--- frontend/src/Components/SignUp/SignUp.jsx
+++ frontend/src/Components/SignUp/SignUp.jsx
@@ -1,18 +1,170 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
-  return (
-    <div className="latte vh-100 d-flex justify-content-center align-items-center">
-      <div className="container text-center">
-        <h1>SIGNUP PAGE!</h1>
-        <Link to="/login" className="btn btn-link btn-lg">
-          To Login Page
-        </Link>
-      </div>
-    </div>
-  );
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [adress, setAdress] = useState("");
+	const [password, setPassword] = useState("");
+	const [confPassword, setConfPassword] = useState("");
+
+	const handleFirstNameChange = (e) => {
+		setFirstName(e.target.value);
+	};
+	const handleLastNameChange = (e) => {
+		setLastName(e.target.value);
+	};
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value);
+	};
+	const handlePhoneChange = (e) => {
+		setPhone(e.target.value);
+	};
+	const handleAdressChange = (e) => {
+		setAdress(e.target.value);
+	};
+	const handlePasswordChange = (e) => {
+		setPassword(e.target.value);
+	};
+	const handleConfPasswordChange = (e) => {
+		setConfPassword(e.target.value);
+	};
+
+	return (
+		<div className="vh-100 container-fluid d-flex justify-content-center align-items-center  latte">
+			<div className="col-sm-10 col-md-7 col-lg-6 col-xl-4 px-5 py-4 border login-form-radius shadow bg-white">
+				<form>
+					<h2 className="text-center">SKY-NEST</h2>
+					<p className="mb-4 p-0 text-center text-secondary">Create your account</p>
+					<p className="alert alert-danger text-danger text-center d-none"></p>
+					<div className="row">
+						<div className="col-md-6">
+							<div className="form-outline mb-1">
+								<input
+									type="name"
+									value={firstName}
+									id="firstNameInput"
+									onChange={handleFirstNameChange}
+									className="form-control form-control-lg"
+									required
+									autoComplete="off"
+								/>
+								<label className="form-label" htmlFor="firstNameInput">
+									First Name <span className="text-danger">*</span>
+								</label>
+							</div>
+						</div>
+						<div className="col-md-6">
+							<div className="form-outline mb-1">
+								<input
+									type="name"
+									value={lastName}
+									onChange={handleLastNameChange}
+									id="lastNameInput"
+									className="form-control form-control-lg"
+									required
+									autoComplete="off"
+								/>
+								<label className="form-label" htmlFor="lastNameInput">
+									Last Name <span className="text-danger">*</span>
+								</label>
+							</div>
+						</div>
+					</div>
+					<div className="form-outline mb-1">
+						<input
+							type="email"
+							value={email}
+							onChange={handleEmailChange}
+							id="emailInput"
+							className="form-control form-control-lg"
+							required
+							autoComplete="off"
+						/>
+						<label className="form-label" htmlFor="emailInput">
+							Email address <span className="text-danger">*</span>
+						</label>
+					</div>
+					<div className="form-outline mb-1">
+						<input
+							type="number"
+							value={phone}
+							onChange={handlePhoneChange}
+							id="phoneInput"
+							className="form-control form-control-lg"
+							required
+							autoComplete="off"
+						/>
+						<label className="form-label" htmlFor="phoneInput">
+							Phone number <span className="text-danger">*</span>
+						</label>
+					</div>
+					<div className="form-outline mb-1">
+						<input
+							type="text"
+							value={adress}
+							onChange={handleAdressChange}
+							id="adressInput"
+							className="form-control form-control-lg"
+							required
+							autoComplete="off"
+						/>
+						<label className="form-label" htmlFor="adressInput">
+							Adress <span className="text-danger">*</span>
+						</label>
+					</div>
+					<div className="row">
+						<div className="col-md-6">
+							<div className="form-outline mb-1">
+								<input
+									type="password"
+									value={password}
+									onChange={handlePasswordChange}
+									id="passwordInput"
+									className="form-control form-control-lg"
+									required
+									autoComplete="off"
+								/>
+								<label className="form-label" htmlFor="passwordInput">
+									Password <span className="text-danger">*</span>
+								</label>
+							</div>
+						</div>
+						<div className="col-md-6">
+							<div className="form-outline mb-1">
+								<input
+									type="password"
+									value={confPassword}
+									onChange={handleConfPasswordChange}
+									id="confPasswordInput"
+									className="form-control form-control-lg"
+									required
+									autoComplete="off"
+								/>
+								<label className="form-label" htmlFor="confPasswordInput">
+									Confirm Password <span className="text-danger">*</span>
+								</label>
+							</div>
+						</div>
+					</div>
+					{
+						<div className="pt-1 mb-1">
+							<button className="btn btn-dark btn-lg btn-block">Sign Up</button>
+						</div>
+					}
+					<div className="mt-4 text-center">
+						<p className="m-0">Already have an account? </p>
+						<Link to={"/login"} href="#!" className="m-0 btn btn-link">
+							Login
+						</Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default SignUp;
