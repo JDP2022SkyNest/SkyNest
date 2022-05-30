d11b47d30e98caa9017db6017f5df56c553d16f6
Marko Radunovic <marko.radunovic@htecgroup.com>
2022-05-30 13:12:56 +0200

    UserValidator completed with initial arguments, Junit5 test class added for Validator, New types of errors added into UserExceptionType

--- backend/src/test/java/com/htecgroup/SkyNest/validator/UserValidatorTest.java
+++ backend/src/test/java/com/htecgroup/SkyNest/validator/UserValidatorTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.platform.commons.util.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
