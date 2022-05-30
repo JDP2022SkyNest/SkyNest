55012d539cce2db7568be41c189744136f2bf826
Darko Veljanovski <darko.veljanoski@htecgroup.com>
2022-05-30 10:45:37 +0200

    little Refactoring

--- backend/src/main/java/com/htecgroup/skynest/security/WebSecurity.java
+++ backend/src/main/java/com/htecgroup/skynest/security/WebSecurity.java
@@ -3,16 +3,13 @@ package com.htecgroup.skynest.security;
 import com.htecgroup.SkyNest.filter.CustomAuthenticationFilter;
 import com.htecgroup.SkyNest.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
