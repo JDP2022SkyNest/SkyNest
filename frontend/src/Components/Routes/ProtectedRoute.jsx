967a78309ac88919954e364bfce5c7eb46727855
casualme2 <miljan.pantovic@htecgroup.com>
2022-05-30 15:28:38 +0200

    PR Fix: Implemented ROUTES to both route files

--- frontend/src/Components/Routes/ProtectedRoute.jsx
+++ frontend/src/Components/Routes/ProtectedRoute.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import ROUTES from "./ROUTES";
 
 const ProtectedRoute = ({ accessToken, children }) => {
 	if (!accessToken) {
-		return <Navigate to={"/login"} />;
+		return <Navigate to={ROUTES.LOGIN} />;
 	}
 	return children;
 };
