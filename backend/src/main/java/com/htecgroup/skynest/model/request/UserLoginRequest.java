5edf0cd34d831e25c1211ac66a2436e97940b3c9
Darko Veljanovski <darko.veljanoski@htecgroup.com>
2022-05-31 09:31:55 +0200

    rm unnecessary getters and setters

--- backend/src/main/java/com/htecgroup/skynest/model/request/UserLoginRequest.java
+++ backend/src/main/java/com/htecgroup/skynest/model/request/UserLoginRequest.java
@@ -1,12 +1,7 @@
 package com.htecgroup.skynest.model.request;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 @Data
-@Setter
-@Getter
 public class UserLoginRequest {
   private String email;
   private String password;
