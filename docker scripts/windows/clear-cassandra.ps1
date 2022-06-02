0f278d6c4c318aa0a41482596454b92a08f6ea9b
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-02 12:47:14 +0200

    fixed windows clear scripts

--- docker scripts/windows/clear-cassandra.ps1
+++ docker scripts/windows/clear-cassandra.ps1
@@ -1,1 +1,10 @@
-bash -c "../unix/clear-cassandra.sh"
+echo "Stopping Cassandra container..."
+docker stop skynest-token-db skynest-token-db-init
+echo "Deleting Cassandra container..."
+docker rm skynest-token-db skynest-token-db-init
+echo "Pruning everything..."
+docker system prune --volumes --force
+echo "Deleting Cassandra cache..."
+rm -Recurse -Force ../../.db_data/cassandra
+Write-Host "Press any key to continue..."
+[void][System.Console]::ReadKey($true)
--- docker scripts/windows/clear-mysql.ps1
+++ docker scripts/windows/clear-mysql.ps1
@@ -1,1 +1,10 @@
-bash -c "../unix/clear-mysql.sh"
+echo "Stopping MySQL container..."
+docker stop skynest-db
+echo "Deleting MySQL container..."
+docker rm skynest-db
+echo "Pruning everything..."
+docker system prune --volumes --force
+echo "Deleting MySQL cache..."
+rm -Recurse -Force ../../.db_data/mysql
+Write-Host "Press any key to continue..."
+[void][System.Console]::ReadKey($true)
