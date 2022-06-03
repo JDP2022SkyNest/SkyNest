d5ea476f0e5d6294caf2d7d92f0a178a151b1c01
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-03 15:14:38 +0200

    cors quickfix

--- backend/src/main/java/com/htecgroup/skynest/SkyNestApplication.java
+++ backend/src/main/java/com/htecgroup/skynest/SkyNestApplication.java
@@ -6,12 +6,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SkyNestApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SkyNestApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+      }
+    };
   }
 
   @Bean
