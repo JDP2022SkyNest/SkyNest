02f3de89344544167c671a60275227e4ab0dd3ab
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-06-06 13:23:54 +0200

    Small refinement

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
-const HomePage = () => {
+const HomePage = ({ setAccessToken }) => {
+   const onLogout = () => {
+      localStorage.removeItem("accessToken");
+      setAccessToken("");
+   };
+
    return (
       <div className="latte vh-100 d-flex justify-content-center align-items-center">
          <div className="container text-center">
             <h1>PLACEHOLDER HOME PAGE</h1>
+            <button className="btn btn-warning" onClick={onLogout}>
+               LOGOUT
+            </button>
          </div>
       </div>
    );
