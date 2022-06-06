aa26bcfa501886efedc1419644e509027410cbb8
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-06 12:22:35 +0200

    clean now also removes backend image to force rebuild

--- docker scripts/unix/clean-all.sh
+++ docker scripts/unix/clean-all.sh
@@ -2,6 +2,8 @@ echo "Removing all containers..."
 docker ps --all --quiet | xargs docker rm
 echo "Pruning everything..."
 docker system prune --volumes --force
+echo "Removing backend image..."
+docker image rm --force skynest/skynest-api
 read -n 1 -s -r -p "Press any key to continue..."
 echo -e "\n"
 
--- docker scripts/unix/stop-and-clean-all.sh
+++ docker scripts/unix/stop-and-clean-all.sh
@@ -2,6 +2,8 @@ echo "Stopping and removing all containers..."
 docker ps --all --quiet | xargs docker stop | xargs docker rm
 echo "Pruning everything..."
 docker system prune --volumes --force
+echo "Removing backend image..."
+docker image rm --force skynest/skynest-api
 read -n 1 -s -r -p "Press any key to continue..."
 echo -e "\n"
 
