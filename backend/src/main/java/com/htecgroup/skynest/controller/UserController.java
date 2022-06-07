ef0fc810c618922d484926c25c26c4baa3da548a
MarkoRadunovicHtec <105654724+MarkoRadunovicHtec@users.noreply.github.com>
2022-06-07 11:20:53 +0200

    SJQ2T-66: Swagger documentation
    * Rebase, added password reset functionality
    
    * Api responses and operation descriptions added for password reset feature
    
    * Added error 500 as expected response

--- backend/src/main/java/com/htecgroup/skynest/controller/UserController.java
+++ backend/src/main/java/com/htecgroup/skynest/controller/UserController.java
@@ -3,8 +3,16 @@ package com.htecgroup.skynest.controller;
 import com.htecgroup.skynest.model.dto.UserDto;
 import com.htecgroup.skynest.model.request.UserPasswordResetRequest;
 import com.htecgroup.skynest.model.request.UserRegisterRequest;
+import com.htecgroup.skynest.model.response.ErrorMessage;
 import com.htecgroup.skynest.model.response.UserResponse;
 import com.htecgroup.skynest.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
@@ -22,11 +30,84 @@ import java.util.stream.Collectors;
 @RequestMapping("/users")
 @AllArgsConstructor
 @Log4j2
+@Tag(name = "User API", description = "Operations to manipulate user")
 public class UserController {
 
   private UserService userService;
   private ModelMapper modelMapper;
 
+  @Operation(summary = "Register new user")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Registration successful",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = UserResponse.class),
+                  examples = {
+                    @ExampleObject(
+                        value =
+                            "{\"id\": \"a6fd6d95-0a60-43ff-961f-2b9b2ff72f95\","
+                                + " \"email\": \"username@gmail.com\","
+                                + "  \"name\": \"Name\","
+                                + "  \"surname\": \"Surname\","
+                                + "  \"phoneNumber\": \"38166575757\","
+                                + "  \"address\": \"Local address\"}")
+                  })
+            }),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Input fields are not in valid format",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorMessage.class),
+                  examples = {
+                    @ExampleObject(
+                        value =
+                            "{\"messages\":[\"password format not valid\","
+                                + " \"name cannot be null or empty\","
+                                + " \"surname cannot be null or empty\","
+                                + " \"email format is not valid\","
+                                + " \"phoneNumber format not valid\"],"
+                                + " \"status\": \"400\","
+                                + " \"timestamp\": \"2022-06-03 16:18:12\"}")
+                  })
+            }),
+        @ApiResponse(
+            responseCode = "409",
+            description = "Email or phone number already in use",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorMessage.class),
+                  examples = {
+                    @ExampleObject(
+                        value =
+                            "{\"messages\":[\"This email is already in use\"],"
+                                + " \"status\": \"409\","
+                                + " \"timestamp\": \"2022-06-03 16:18:12\"}")
+                  })
+            }),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+      })
+  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+      content =
+          @Content(
+              schema = @Schema(implementation = UserRegisterRequest.class),
+              mediaType = "application/json",
+              examples = {
+                @ExampleObject(
+                    value =
+                        "{\"email\": \"username@gmail.com\","
+                            + " \"password\": \"paSword1\","
+                            + "  \"name\": \"Name\","
+                            + "  \"surname\": \"Surname\","
+                            + "  \"phoneNumber\": \"38166575757\","
+                            + "  \"address\": \"Local address\"}")
+              }))
   @PostMapping("/register")
   public ResponseEntity<UserResponse> registerUser(
       @Valid @RequestBody UserRegisterRequest userRegisterRequest) {
@@ -36,6 +117,32 @@ public class UserController {
     return new ResponseEntity<>(modelMapper.map(userDto, UserResponse.class), HttpStatus.OK);
   }
 
+  @Operation(summary = "Confirm email")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Email confirmed",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "User verified successfully")})
+            }),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Email confirmation failed",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {
+                    @ExampleObject(
+                        value =
+                            "Jwt token failed the validation. For more information check the logger")
+                  })
+            })
+      })
   @GetMapping("/confirm")
   public ResponseEntity<String> confirmEmail(@RequestParam String token) {
     String response = userService.confirmEmail(token);
@@ -43,6 +150,29 @@ public class UserController {
     return ResponseEntity.ok(response);
   }
 
+  @Operation(summary = "Resend email for verification")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Email resent",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Email resent successfully")})
+            }),
+        @ApiResponse(
+            responseCode = "500",
+            description =
+                "User already registered, not a valid email address or failed to resend email",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Failed to send email")})
+            })
+      })
   @PostMapping("/resend-email")
   public ResponseEntity<String> resendUserEmail(@RequestParam String email) {
     userService.sendVerificationEmail(email);
@@ -51,6 +181,46 @@ public class UserController {
     return ResponseEntity.ok(response);
   }
 
+  @Operation(summary = "Request for password reset")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Password reset email successfully sent",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Password reset email sent")})
+            }),
+        @ApiResponse(
+            responseCode = "400",
+            description = "Email does not exist",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "This email is not in use")})
+            }),
+        @ApiResponse(
+            responseCode = "403",
+            description = "Access denied",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Access token is invalid")})
+            }),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Not a valid email address or failed to send email",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Failed to send email")})
+            })
+      })
   @PostMapping("/password-reset/request")
   public ResponseEntity<String> requestPasswordReset(@RequestParam String email) {
     userService.sendPasswordResetEmail(email);
@@ -59,6 +229,30 @@ public class UserController {
     return ResponseEntity.ok(response);
   }
 
+  // Password was successfully reset
+  @Operation(summary = "Confirm password reset")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Password reset confirmed",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Password was successfully reset")})
+            }),
+        @ApiResponse(
+            responseCode = "403",
+            description = "Access denied",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = String.class),
+                  examples = {@ExampleObject(value = "Access token is invalid")})
+            }),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+      })
   @PutMapping("/password-reset/confirm")
   public ResponseEntity<String> confirmPasswordReset(
       @Valid @RequestBody UserPasswordResetRequest userPasswordResetRequest) {
@@ -69,6 +263,7 @@ public class UserController {
     return ResponseEntity.ok(response);
   }
 
+  @Operation(summary = "Get User with that id")
   @GetMapping
   public List<UserResponse> getUsers() {
     List<UserDto> listOfUsers = userService.listAllUsers();
