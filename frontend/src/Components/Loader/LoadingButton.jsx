18d0181665691f58fa4707cae4101be22c34aa9d
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-06-07 11:53:10 +0200

    PR FIX: closed span to be more visually appealing

--- frontend/src/Components/Loader/LoadingButton.jsx
+++ frontend/src/Components/Loader/LoadingButton.jsx
@@ -4,7 +4,7 @@ const LoadingButton = () => {
    return (
       <div className="pt-1">
          <button className="btn btn-dark btn-lg btn-block d-flex align-items-center justify-content-center" disabled>
-            <span className="spinner-border spinner-border-md"></span>
+            <span className="spinner-border spinner-border-md" />
          </button>
       </div>
    );
