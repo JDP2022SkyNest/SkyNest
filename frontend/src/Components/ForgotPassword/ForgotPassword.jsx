a8103138bbda6f0762a3253127d39d9ebfabbb9b
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-06-07 09:26:14 +0200

    Added: Right endpoint and main fuctionality

--- frontend/src/Components/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/Components/ForgotPassword/ForgotPassword.jsx
@@ -1,40 +1,66 @@
 import React, { useEffect, useRef, useState } from "react";
 import ROUTES from "../Routes/ROUTES";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CenteredContainer from "../ReusableComponents/CenteredContainer";
 import AxiosInstance from "../axios/AxiosInstance";
+import { inputsDisabled } from "../ReusableComponents/ReusableFunctions";
 
 const ForgotPassword = () => {
    const [email, setEmail] = useState("");
+   const [errorMsg, setErorrMsg] = useState("");
+   const [successMsg, setSuccessMsg] = useState("");
+   const [loading, setLoading] = useState(false);
+
    const emailRef = useRef();
+   const navigate = useNavigate();
+   const allInputs = document.querySelectorAll("input");
+
+   const redirectToLoginPage = (delay) => {
+      setTimeout(() => {
+         navigate(ROUTES.LOGIN);
+      }, delay);
+   };
 
    const onEmailChange = (e) => {
       setEmail(e.target.value);
    };
 
-   const onFormSubmit = (e) => {
+   const onFormSubmit = async (e) => {
       e.preventDefault();
-      onSuccessfulChange();
+      setLoading(true);
+      inputsDisabled(allInputs, true);
+      await onSuccessfulChange();
+      inputsDisabled(allInputs, false);
    };
 
    const onSuccessfulChange = async () => {
       try {
-         await AxiosInstance.post("/users/password-reset/request", { email });
-         console.log("SUCCESS");
-      } catch (err) {
-         console.log(err);
+         await AxiosInstance.post(`/users/password-reset/request?email=${email}`);
+         setSuccessMsg("Email has been sent");
+         redirectToLoginPage(2000);
+      } catch ({ response }) {
+         if (response.status === 400) {
+            setErorrMsg("Email not found");
+         }
+         setLoading(false);
       }
    };
 
    useEffect(() => {
       emailRef.current.focus();
-   });
+   }, []);
+
+   useEffect(() => {
+      setErorrMsg("");
+   }, [email]);
 
    return (
       <CenteredContainer>
          <form onSubmit={onFormSubmit}>
             <h5 className="mb-4 p-0 text-center text-dark">Please enter your e-mail</h5>
             <div className="form-outline mb-4">
+               <p className={errorMsg ? "alert alert-danger text-danger text-center" : "d-none"}>{errorMsg}</p>
+               <p className={successMsg ? "alert alert-success text-success text-center" : "d-none"}>{successMsg}</p>
                <label className="form-label" htmlFor="emailInput">
                   Email address
                </label>
@@ -50,7 +76,17 @@ const ForgotPassword = () => {
                />
             </div>
             <div className="pt-1 mb-4">
-               <button className="btn btn-dark btn-lg btn-block">Submit</button>
+               {loading ? (
+                  <div className="pt-1">
+                     <button className="btn btn-dark btn-lg btn-block d-flex align-items-center justify-content-center" disabled>
+                        <span className="spinner-border spinner-border-md"></span>
+                     </button>
+                  </div>
+               ) : (
+                  <div className="pt-1 mb-4">
+                     <button className="btn btn-dark btn-lg btn-block">Submit</button>
+                  </div>
+               )}
             </div>
             <div className="mt-4 text-center">
                <p className="m-0">Wan't to go back? </p>
