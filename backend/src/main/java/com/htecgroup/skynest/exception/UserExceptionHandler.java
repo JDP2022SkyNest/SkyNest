aff966b7f0690458471717a4795e8b6a96b9da2d
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-07 14:45:14 +0200

    refactored error handling in auth filter

--- backend/src/main/java/com/htecgroup/skynest/exception/UserExceptionHandler.java
+++ backend/src/main/java/com/htecgroup/skynest/exception/UserExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.htecgroup.skynest.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.htecgroup.skynest.model.response.ErrorMessage;
 import com.htecgroup.skynest.util.DateTimeUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,44 +11,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
 public class UserExceptionHandler {
 
-  public static ResponseEntity<ErrorMessage> responseFor(UserException ex) {
-    return new UserExceptionHandler().handleUserException(ex, null);
-  }
-
-  public static void writeToResponse(
-      ResponseEntity<ErrorMessage> responseEntity, HttpServletResponse response)
-      throws IOException {
-    for (Map.Entry<String, List<String>> header : responseEntity.getHeaders().entrySet()) {
-      String headerName = header.getKey();
-      for (String headerValue : header.getValue()) {
-        response.addHeader(headerName, headerValue);
-      }
-    }
-    response.setStatus(responseEntity.getStatusCodeValue());
-
-    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-    String jsonBody = new ObjectMapper().writeValueAsString(responseEntity.getBody());
-    response.getWriter().write(jsonBody);
-    response.flushBuffer();
-  }
-
   @ExceptionHandler(value = {UserException.class})
   public ResponseEntity<ErrorMessage> handleUserException(UserException ex, WebRequest webRequest) {
-    ErrorMessage errorMessage =
-        new ErrorMessage(
-            ex.getMessage(), ex.getStatus().value(), DateTimeUtil.currentTimeFormatted());
     log.error("Handle UserException. Error: {}", ex.getMessage());
-    return new ResponseEntity<>(errorMessage, ex.getStatus());
+    return ex.asResponseEntity();
   }
 
   @ExceptionHandler(value = {MethodArgumentNotValidException.class})
--- backend/src/main/java/com/htecgroup/skynest/filter/CustomAuthorizationFilter.java
+++ backend/src/main/java/com/htecgroup/skynest/filter/CustomAuthorizationFilter.java
@@ -2,12 +2,9 @@ package com.htecgroup.skynest.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.htecgroup.skynest.exception.UserException;
-import com.htecgroup.skynest.exception.UserExceptionHandler;
-import com.htecgroup.skynest.model.response.ErrorMessage;
 import com.htecgroup.skynest.util.JwtUtils;
 import com.htecgroup.skynest.util.UrlUtil;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -49,8 +46,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
       filterChain.doFilter(request, response);
     } catch (UserException ex) {
-      ResponseEntity<ErrorMessage> responseEntity = UserExceptionHandler.responseFor(ex);
-      UserExceptionHandler.writeToResponse(responseEntity, response);
+      ex.writeToResponse(response);
     } catch (Exception e) {
       log.error(e);
       new ObjectMapper().writeValue(response.getOutputStream(), "Internal Error.");
