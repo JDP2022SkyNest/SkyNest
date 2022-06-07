5b1c3dfa8494a193e77dce0f366914ffce63befc
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-07 13:47:17 +0200

    concat with string format

--- backend/src/main/java/com/htecgroup/skynest/filter/CustomAuthenticationFilter.java
+++ backend/src/main/java/com/htecgroup/skynest/filter/CustomAuthenticationFilter.java
@@ -84,7 +84,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         JwtUtils.generate(
             user.getUsername(), JwtUtils.ACCESS_TOKEN_EXPIRATION_MS, "roles", authorities);
 
-    response.addHeader(JwtUtils.AUTH_HEADER, JwtUtils.TOKEN_PREFIX + token);
+    response.addHeader(JwtUtils.AUTH_HEADER, String.format("%s%s", JwtUtils.TOKEN_PREFIX, token));
     log.info("Jwt token successfully created for user: {}", user.getUsername());
 
     log.info("{} is successfully logged in.", user.getUsername());
