02990320b62f1736c3b519e684172a47a1a38344
MilenaHtec <milena.statkic@htecgroup.com>
2022-06-10 11:12:55 +0200

    Navbar Component

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+import "./Navbar.css";
+
+import Sidebar from "../Sidebar/Sidebar";
+import Profile from "../Profile/Profile";
+import logoImage from "..//Login/assets/logoblackandwhite.svg";
+
+function Navbar() {
+   return (
+      <Nav className="navbar d-flex  ">
+         <h1 className="brand">Skynest</h1>
+         <img src={logoImage} alt="logo" className="logoImageBrand" />
+         <Sidebar />
+         <Profile />
+      </Nav>
+   );
+}
+
+export default Navbar;
