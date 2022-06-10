79ba81a19e2f589b035e566439ed3b09c546de7e
MilenaHtec <milena.statkic@htecgroup.com>
2022-06-10 11:25:16 +0200

    Creating pages for Profile sidebar

---
+++ frontend/src/Components/pages/YourProfile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CenteredContainer from "../ReusableComponents/CenteredContainer";
+
+function YourProfile() {
+   return <CenteredContainer>Your Profile</CenteredContainer>;
+}
+
+export default YourProfile;
