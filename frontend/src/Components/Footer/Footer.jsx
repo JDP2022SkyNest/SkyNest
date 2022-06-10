c70372829b258872478f47900411f0760b146d24
MilenaHtec <milena.statkic@htecgroup.com>
2022-06-10 11:09:20 +0200

    Footer component

---
+++ frontend/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+   return (
+      <footer className="navbar bg-dark text-white text-dark fixed-bottom row footer mt-5">
+         <div className="container-fluid">
+            <div className="p-1 m-auto">© 2022 Copyright SkyNest</div>
+         </div>
+      </footer>
+   );
+};
+export default Footer;
