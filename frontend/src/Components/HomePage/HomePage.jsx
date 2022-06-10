b81d5debd24b395ddc1558cecdd69f534ebd5c05
MilenaHtec <milena.statkic@htecgroup.com>
2022-06-10 11:16:19 +0200

    Creating Sidebars

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "..//HomePage/HomePage.css";
 import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
 import CenteredContainer from "..//ReusableComponents/CenteredContainer";
 
 const HomePage = () => {
    return (
       <>
+         <Navbar />
          <CenteredContainer>HOMEPAGE</CenteredContainer>
          <Footer />
       </>
