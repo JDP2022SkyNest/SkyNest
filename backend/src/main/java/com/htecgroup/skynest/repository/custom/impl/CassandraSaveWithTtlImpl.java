aa495bb7a60ac1ae96dc7c9038c59c8bb6b83404
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-13 15:51:53 +0200

    removed pointless validation

--- backend/src/main/java/com/htecgroup/skynest/repository/custom/impl/CassandraSaveWithTtlImpl.java
+++ backend/src/main/java/com/htecgroup/skynest/repository/custom/impl/CassandraSaveWithTtlImpl.java
@@ -4,7 +4,6 @@ import com.htecgroup.skynest.repository.custom.CassandraSaveWithTtl;
 import lombok.AllArgsConstructor;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.core.InsertOptions;
-import org.springframework.util.Assert;
 
 @AllArgsConstructor
 public class CassandraSaveWithTtlImpl<T> implements CassandraSaveWithTtl<T> {
@@ -13,10 +12,6 @@ public class CassandraSaveWithTtlImpl<T> implements CassandraSaveWithTtl<T> {
 
   @Override
   public <S extends T> S save(S entity, int timeToLiveInSeconds) {
-
-    Assert.notNull(entity, "Entity must not be null");
-    Assert.isTrue(timeToLiveInSeconds >= 0, "Time to live must be greater than zero");
-
     InsertOptions insertOptions =
         InsertOptions.builder().ttl(timeToLiveInSeconds).ifNotExists(true).build();
     operations.insert(entity, insertOptions);
