87a4b89f9cdb935017a012ca96eeb7e86d0075d8
MetoMladenovski <105630797+MetoMladenovski@users.noreply.github.com>
2022-06-15 11:32:41 +0200

    SJQ2T-95 Testing UserServiceImpl (#45)
    * Fixed some tests and added new ones.
    
    * Made some changes and added utils in tests.
    
    * Excluded model mapper from tests.

---
+++ backend/src/test/java/com/htecgroup/skynest/utils/BasicUtil.java
@@ -0,0 +1,24 @@
+package com.htecgroup.skynest.utils;
+
+import com.htecgroup.skynest.model.dto.RoleDto;
+import com.htecgroup.skynest.model.entity.RoleEntity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class BasicUtil {
+
+  protected static final LocalDateTime currentDateTime = LocalDateTime.now();
+  protected static final RoleEntity roleWorkerEntity =
+      new RoleEntity(UUID.randomUUID(), RoleEntity.ROLE_WORKER);
+  protected static final RoleDto roleWorkerDto =
+      new RoleDto(UUID.randomUUID(), RoleEntity.ROLE_WORKER);
+  protected static String email = "test@test.com";
+  protected static String password = "password";
+  protected static String encryptedPassoword =
+      "$2a$12$R9h/cIPz0gi.URNNX3kh2OPST9/PgBkqquzi.Ss7KIUgO2t0jWMUW";
+  protected static String name = "Name";
+  protected static String surname = "Surname";
+  protected static String address = "Address";
+  protected static String phoneNumber = "381123456789";
+}
