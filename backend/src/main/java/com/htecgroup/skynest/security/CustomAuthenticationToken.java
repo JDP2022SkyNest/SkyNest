6ed6744b6632fb24afa68f703dd6670231bbd9bc
Marko Radunovic <marko.radunovic@htecgroup.com>
2022-06-16 16:15:20 +0200

    LoggedUserDto instead of Object return value fixed

--- backend/src/main/java/com/htecgroup/skynest/security/CustomAuthenticationToken.java
+++ backend/src/main/java/com/htecgroup/skynest/security/CustomAuthenticationToken.java
@@ -25,7 +25,7 @@ public class CustomAuthenticationToken extends UsernamePasswordAuthenticationTok
   }
 
   @Override
-  public Object getPrincipal() {
+  public LoggedUserDto getPrincipal() {
     return loggedUserDto;
   }
 }
