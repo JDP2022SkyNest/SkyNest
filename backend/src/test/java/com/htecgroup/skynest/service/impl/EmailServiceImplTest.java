1231e885a4e842bbf75442ed689527aec3460906
Dragan Mitrasinovic <dragan.mitrasinovic@htecgroup.com>
2022-06-16 15:02:59 +0200

    Fixed existing tests according to my changes

--- backend/src/test/java/com/htecgroup/skynest/service/impl/EmailServiceImplTest.java
+++ backend/src/test/java/com/htecgroup/skynest/service/impl/EmailServiceImplTest.java
@@ -1,7 +1,11 @@
 package com.htecgroup.skynest.service.impl;
 
+import com.auth0.jwt.algorithms.Algorithm;
 import com.htecgroup.skynest.exception.UserException;
 import com.htecgroup.skynest.model.email.Email;
+import com.htecgroup.skynest.util.EmailUtil;
+import com.htecgroup.skynest.util.JwtUtils;
+import com.htecgroup.skynest.utils.UserDtoUtil;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.TemplateEngine;
 
 import javax.mail.Address;
 import javax.mail.Message;
@@ -18,6 +23,8 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,6 +32,7 @@ import static org.mockito.Mockito.when;
 class EmailServiceImplTest {
 
   @Mock private JavaMailSender javaMailSender;
+  @Mock private TemplateEngine templateEngine;
 
   @InjectMocks private EmailServiceImpl emailService;
 
@@ -34,15 +42,16 @@ class EmailServiceImplTest {
 
   @BeforeEach
   void setUp() {
-    String to = "test@yahoo.com";
-    String emailText = "MailContent";
-    String subject = "Confirm your email for SkyNest";
-    this.email = new Email(to, subject, emailText, true);
+    JwtUtils.ALGORITHM = Algorithm.HMAC512("test secret");
+    String token = JwtUtils.generateEmailVerificationToken(UserDtoUtil.getNotVerified().getEmail());
+    email = EmailUtil.createVerificationEmail(UserDtoUtil.getNotVerified(), token);
   }
 
   @Test
   void sendTest() {
+    String emailText = "Email text";
     MimeMessage mimeMessage = mock(MimeMessage.class);
+    when(templateEngine.process(anyString(), any())).thenReturn(emailText);
     when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
     emailService.send(email);
     Mockito.verify(javaMailSender).send(mimeMessage);
@@ -50,18 +59,20 @@ class EmailServiceImplTest {
 
   @Test
   void sendFailsTest() {
-    Email mockedEmail = mock(Email.class);
+    String emailText = "Email text";
+    when(templateEngine.process(anyString(), any())).thenReturn(emailText);
     when(javaMailSender.createMimeMessage()).thenThrow(UserException.class);
-    Assertions.assertThrows(UserException.class, () -> emailService.send(mockedEmail));
+    Assertions.assertThrows(UserException.class, () -> emailService.send(email));
   }
 
   @Test
   void testMimeMessageHelperArgumentsInSend() throws MessagingException, IOException {
-    String expectedTo = "test@yahoo.com";
-    String expectedEmailText = "MailContent";
-    String expectedSubject = "Confirm your email for SkyNest";
+    String expectedTo = email.getTo();
+    String expectedEmailText = "Email text";
+    String expectedSubject = email.getEmailType().getSubject();
     MimeMessage mimeMessage = new MimeMessage((Session) null);
     when(javaMailSender.createMimeMessage()).thenReturn(mimeMessage);
+    when(templateEngine.process(anyString(), any())).thenReturn(expectedEmailText);
     emailService.send(email);
 
     Mockito.verify(javaMailSender).send(captorMimeMessage.capture());
