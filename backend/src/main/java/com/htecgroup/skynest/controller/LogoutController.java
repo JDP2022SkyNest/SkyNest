6f90e49f4ef833b6164043499fde6d763d3304d2
MetoMladenovski <metodi.mladenovski@htecgroup.com>
2022-06-16 13:49:20 +0200

    Using RequestHeader instead of hole request

--- backend/src/main/java/com/htecgroup/skynest/controller/LogoutController.java
+++ backend/src/main/java/com/htecgroup/skynest/controller/LogoutController.java
@@ -13,10 +13,9 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/auth/logout")
@@ -41,8 +40,8 @@ public class LogoutController {
             }),
       })
   @PostMapping
-  public ResponseEntity<String> logout(HttpServletRequest request) {
-    String authorizationHeader = request.getHeader(JwtUtils.AUTH_HEADER);
+  public ResponseEntity<String> logout(
+      @RequestHeader(name = "Authorization") String authorizationHeader) {
     String token = authorizationHeader.replace(JwtUtils.TOKEN_PREFIX, "");
     invalidJwtService.invalidate(token);
     return ResponseEntity.ok("User logged out successfully");
