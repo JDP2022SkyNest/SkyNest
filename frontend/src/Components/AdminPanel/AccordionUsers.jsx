042a2c0e9ba2c5c3434d656d1f8409b5ce2eed46
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-06-17 13:04:00 +0200

    Small changes

--- frontend/src/Components/AdminPanel/AccordionUsers.jsx
+++ frontend/src/Components/AdminPanel/AccordionUsers.jsx
@@ -32,20 +32,26 @@ const AccordionUsers = ({ elem, index, deleteUser, accessToken }) => {
                {elem.address}
             </p>
             <p>
-               <span className="text-warning font-weight-bold">User ID: </span>
+               <span className="font-weight-bold">User ID: </span>
                {elem.id}
             </p>
-            <button
-               onClick={() => {
-                  deleteUser(accessToken, elem.id);
-               }}
-               className="btn btn-danger"
-            >
-               Delete
-            </button>
-            {/* This button is still a placeholder, functinality will be added */}
-            <button className="btn btn-success ml-2">Promote</button>
-            <button className="btn btn-warning ml-2">Demote</button>
+            <div className="d-flex justify-content-between">
+               <div>
+                  {/* This button is still a placeholder, functinality will be added */}
+                  <button className="btn btn-info text-white">Promote</button>
+                  <button className="btn btn-primary ml-2">Demote</button>
+               </div>
+               <div>
+                  <button
+                     onClick={() => {
+                        deleteUser(accessToken, elem.id);
+                     }}
+                     className="btn btn-danger"
+                  >
+                     Delete
+                  </button>
+               </div>
+            </div>
          </Accordion.Body>
       </Accordion.Item>
    );
