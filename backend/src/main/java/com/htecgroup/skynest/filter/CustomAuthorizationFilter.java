cb61566bce865620976ce6bd6ee95cd258d1529c
MetoMladenovski <105630797+MetoMladenovski@users.noreply.github.com>
2022-06-17 13:40:42 +0200

    SJQ2T-76: Invalidate access tokens (#60)
    * Created LogoutController for logout, added check for invalid token in authorization filter.
    
    * Edited swagger documentation.
    
    * Merged dev and added logs.
    
    * Fixed logs, changed logout path
    
    * Using RequestHeader instead of hole request
    
    * Merged dev, deleted comments.

--- backend/src/main/java/com/htecgroup/skynest/filter/CustomAuthorizationFilter.java
+++ backend/src/main/java/com/htecgroup/skynest/filter/CustomAuthorizationFilter.java
@@ -5,6 +5,7 @@ import com.htecgroup.skynest.model.dto.LoggedUserDto;
 import com.htecgroup.skynest.model.response.ErrorMessage;
 import com.htecgroup.skynest.security.CustomAuthenticationToken;
 import com.htecgroup.skynest.security.CustomUserDetailsService;
+import com.htecgroup.skynest.service.InvalidJwtService;
 import com.htecgroup.skynest.util.DateTimeUtil;
 import com.htecgroup.skynest.util.ExceptionUtil;
 import com.htecgroup.skynest.util.JwtUtils;
@@ -25,6 +26,7 @@ import java.io.IOException;
 @Log4j2
 @AllArgsConstructor
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
+  private InvalidJwtService invalidJwtService;
 
   private CustomUserDetailsService customUserDetailsService;
 
@@ -51,8 +53,16 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
         return;
       }
-
       String token = authorizationHeader.replace(JwtUtils.TOKEN_PREFIX, "");
+
+      if (invalidJwtService.isInvalid(token)) {
+        filterChain.doFilter(request, response);
+        log.info(
+            "Token {} is invalid, because it's present in invalidated tokens database.", token);
+        return;
+      }
+
+      SecurityContextHolder.getContext().setAuthentication(JwtUtils.getFrom(token));
       UsernamePasswordAuthenticationToken authToken = JwtUtils.getFrom(token);
       LoggedUserDto loggedUserDto =
           (LoggedUserDto)
