abc0fc2d17d4b598de0d152af1e876631271e4b4
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-20 16:59:36 +0200

    procedure now 'returns' how many rows were updated

--- backend/src/main/resources/db/migration/V2.10__procedure_change_user_role.sql
+++ backend/src/main/resources/db/migration/V2.10__procedure_change_user_role.sql
@@ -1,14 +1,12 @@
 DELIMITER //
-CREATE PROCEDURE `change_user_role` (IN user_email CHAR(254), IN new_role CHAR(255))
+CREATE PROCEDURE `change_user_role` (IN user_email CHAR(254), IN new_role CHAR(255), OUT update_count INT)
 BEGIN
-    DECLARE updateCount INT DEFAULT 0;
-
     UPDATE user
     SET role_id = (SELECT id FROM role WHERE name = new_role)
     WHERE email = user_email;
 
-    SET updateCount = (SELECT ROW_COUNT());
-    CASE updateCount
+    SET update_count = (SELECT ROW_COUNT());
+    CASE update_count
             WHEN 1 THEN SELECT concat('changed ', user_email, ' to ', new_role) AS 'OPERATION RESULT';
             ELSE SELECT 'no changes' AS 'OPERATION RESULT';
     END CASE;
--- docker scripts/unix/change-user-role.sh
+++ docker scripts/unix/change-user-role.sh
@@ -5,7 +5,7 @@ echo -e "\trole_admin"
 read -p "new role: role_" role
 read -p "email: " username
 echo "--DB password"
-docker exec -it skynest-db mysql -u user -p --execute="USE skynest; CALL change_user_role('$username','role_$role')"
+docker exec -it skynest-db mysql -u user -p --execute="USE skynest; CALL change_user_role('$username','role_$role',@updated_count)"
 read -n 1 -s -r -p "Press any key to continue..."
 echo -e "\n"
 
