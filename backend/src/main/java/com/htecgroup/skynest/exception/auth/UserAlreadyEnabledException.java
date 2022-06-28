8eacc85bb2f65487354f95ea77186d7945c1ce89
MetoMladenovski <metodi.mladenovski@htecgroup.com>
2022-06-28 14:06:19 +0200

    added tests for enable user.

--- backend/src/main/java/com/htecgroup/skynest/exception/auth/UserAlreadyEnabledException.java
+++ backend/src/main/java/com/htecgroup/skynest/exception/auth/UserAlreadyEnabledException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class UserAlreadyEnabledException extends AuthException {
 
-  private static final String MESSAGE = "User is already enabled";
+  public static final String MESSAGE = "User is already enabled";
 
   public UserAlreadyEnabledException() {
     super(MESSAGE, HttpStatus.CONFLICT);
