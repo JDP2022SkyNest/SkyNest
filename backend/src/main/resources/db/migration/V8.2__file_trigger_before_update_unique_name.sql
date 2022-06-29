52ff1bf98795f06137e8b8b58d7be50c15a88d7f
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-06-29 16:23:17 +0200

    fixed update triggers

--- backend/src/main/resources/db/migration/V8.2__file_trigger_before_update_unique_name.sql
+++ backend/src/main/resources/db/migration/V8.2__file_trigger_before_update_unique_name.sql
@@ -3,8 +3,10 @@ CREATE TRIGGER `file_before_update_unique_name`
   BEFORE UPDATE ON `file`
   FOR EACH ROW
 BEGIN
-  IF EXISTS(SELECT * FROM `file` f JOIN `object` o ON (f.id=o.id) WHERE o.name=(SELECT o1.name FROM `object` o1 WHERE o1.id = NEW.id) AND f.parent_folder_id<=>NEW.parent_folder_id AND f.bucket_id=NEW.bucket_id) THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'File name must be unique within a folder';
+  IF NOT (OLD.parent_folder_id<=>NEW.parent_folder_id AND OLD.bucket_id=NEW.bucket_id) THEN
+    IF EXISTS(SELECT * FROM `file` f JOIN `object` o ON (f.id=o.id) WHERE o.name=(SELECT o1.name FROM `object` o1 WHERE o1.id = NEW.id) AND f.parent_folder_id<=>NEW.parent_folder_id AND f.bucket_id=NEW.bucket_id) THEN
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'File name must be unique within a folder';
+    END IF;
   END IF;
 END//
 DELIMITER ;
--- backend/src/main/resources/db/migration/V8.5__object_trigger_before_update_unique_filename.sql
+++ backend/src/main/resources/db/migration/V8.5__object_trigger_before_update_unique_filename.sql
@@ -3,8 +3,10 @@ CREATE TRIGGER `object_before_update_unique_filename`
   BEFORE UPDATE ON `object`
   FOR EACH ROW
 BEGIN
-  IF EXISTS(SELECT * FROM `object` o JOIN `file` f ON (o.id=f.id) WHERE o.name=NEW.name AND f.parent_folder_id<=>(SELECT f1.parent_folder_id FROM `file` f1 WHERE f1.id=NEW.id) AND f.bucket_id=(SELECT f2.bucket_id FROM `file` f2 WHERE f2.id=NEW.id)) THEN
-    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'File name must be unique within a folder';
+  IF NOT (OLD.name=NEW.name) THEN
+    IF EXISTS(SELECT * FROM `object` o JOIN `file` f ON (o.id=f.id) WHERE o.name=NEW.name AND f.parent_folder_id<=>(SELECT f1.parent_folder_id FROM `file` f1 WHERE f1.id=NEW.id) AND f.bucket_id=(SELECT f2.bucket_id FROM `file` f2 WHERE f2.id=NEW.id)) THEN
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'File name must be unique within a folder';
+    END IF;
   END IF;
 END//
 DELIMITER ;
