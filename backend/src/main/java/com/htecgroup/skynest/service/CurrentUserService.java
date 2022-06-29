d7afa8b832f8766fa622a505efa082112072fa46
MetoMladenovski <metodi.mladenovski@htecgroup.com>
2022-06-29 12:13:25 +0200

    implemented get logged user.

--- backend/src/main/java/com/htecgroup/skynest/service/CurrentUserService.java
+++ backend/src/main/java/com/htecgroup/skynest/service/CurrentUserService.java
@@ -1,8 +1,11 @@
 package com.htecgroup.skynest.service;
 
 import com.htecgroup.skynest.model.dto.LoggedUserDto;
+import com.htecgroup.skynest.model.response.LoggedUserResponse;
 
 public interface CurrentUserService {
 
   LoggedUserDto getLoggedUser();
+
+  LoggedUserResponse getUserResponseForLoggedUser();
 }
