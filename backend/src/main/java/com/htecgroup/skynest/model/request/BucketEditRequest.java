c14ddbe80d9c18b3990d8b18c114224e0e176bff
Marko Radunovic <marko.radunovic@htecgroup.com>
2022-07-01 14:00:23 +0200

    BucketResponse fixed isPublic field

--- backend/src/main/java/com/htecgroup/skynest/model/request/BucketEditRequest.java
+++ backend/src/main/java/com/htecgroup/skynest/model/request/BucketEditRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
@@ -21,7 +21,6 @@ public class BucketEditRequest {
   @Size(max = 5000, message = "length cannot be over 5000 characters")
   private String description;
 
-  @NotBlank(message = "cannot be null or empty")
-  @Pattern(regexp = "true|false", message = "must be true or false")
-  private String isPublic;
+  @NotNull(message = "must be true or false")
+  private Boolean isPublic;
 }
