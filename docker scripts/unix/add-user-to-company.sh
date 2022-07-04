4b4c66f9354281510099a6460d3b3a0d105cc11f
Dragan Mitrasinovic <dragan.mitrasinovic@htecgroup.com>
2022-07-04 15:28:11 +0200

    Showing operation result in terminal

--- docker scripts/unix/add-user-to-company.sh
+++ docker scripts/unix/add-user-to-company.sh
@@ -3,7 +3,9 @@ read -p "email: " email
 echo "Pib of the company you want to add him to"
 read -p "pib: " pib
 echo "--DB password"
-docker exec -it skynest-db mysql -u user -p --execute="USE skynest; UPDATE user SET company_id = (SELECT id FROM company WHERE pib = '$pib') WHERE email = '$email';
-							SELECT id, name, surname, email, company_id FROM user WHERE email = '$email';"
+docker exec -it skynest-db mysql -u user -p --execute="USE skynest; 
+							UPDATE user SET company_id = (SELECT id FROM company WHERE pib = '$pib') WHERE email = '$email';
+							SET @update_count = (SELECT ROW_COUNT());
+							SELECT CASE @update_count WHEN 1 THEN 'User $email added to company $pib'  ELSE 'no changes' END AS 'OPERATION RESULT';"
 read -n 1 -s -r -p "Press any key to continue..."
 echo -e "\n"
