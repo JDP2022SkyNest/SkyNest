35b37b9cfd000c1ceee8595af094521dd989019c
Marko Radunovic <marko.radunovic@htecgroup.com>
2022-07-04 17:17:22 +0200

    Added fields in FolderResponse

--- backend/src/main/java/com/htecgroup/skynest/model/response/FolderResponse.java
+++ backend/src/main/java/com/htecgroup/skynest/model/response/FolderResponse.java
@@ -7,6 +7,9 @@ import java.util.UUID;
 @Data
 public class FolderResponse {
   private UUID id;
+  private String createdOn;
+  private String modifiedOn;
+  private String deletedOn;
   private String name;
   private String createdById;
   private String parentFolderId;
