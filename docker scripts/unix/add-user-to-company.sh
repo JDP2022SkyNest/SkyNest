b39dc6d66c235d963c0e836b9f6544f7abdbc5f7
Dragan Mitrasinovic <dragan.mitrasinovic@htecgroup.com>
2022-07-04 16:12:00 +0200

    Showing operation result in terminal

--- docker scripts/unix/add-user-to-company.sh
+++ docker scripts/unix/add-user-to-company.sh
@@ -6,6 +6,6 @@ echo "--DB password"
 docker exec -it skynest-db mysql -u user -p --execute="USE skynest; 
 							UPDATE user SET company_id = (SELECT id FROM company WHERE pib = '$pib') WHERE email = '$email';
 							SET @update_count = (SELECT ROW_COUNT());
-							SELECT CASE @update_count WHEN 1 THEN 'User $email added to company $pib'  ELSE 'no changes' END AS 'OPERATION RESULT';"
+							SELECT CASE @update_count WHEN 1 THEN 'User $email added to company $pib' ELSE 'no changes' END AS 'OPERATION RESULT';"
 read -n 1 -s -r -p "Press any key to continue..."
 echo -e "\n"
