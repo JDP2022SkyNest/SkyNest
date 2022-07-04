bd38d6b5b1b64ae4829109638be056a7096cdd91
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-07-04 10:03:20 +0200

    Added Loader

--- frontend/src/Components/CompanyInfo/CompanyInfo.jsx
+++ frontend/src/Components/CompanyInfo/CompanyInfo.jsx
@@ -3,15 +3,21 @@ import { useState } from "react";
 import Footer from "../Footer/Footer";
 import NavbarPanel from "../ReusableComponents/NavbarPanel";
 import ROUTES from "../Routes/ROUTES";
+import LoaderAnimation from "../Loader/LoaderAnimation";
 import "./CompanyInfo.css";
 
 const CompanyInfo = () => {
    const [edit, setEdit] = useState(false);
+   const [loading, setLoading] = useState(false);
+
    return (
       <div className="company-page-body">
-         <NavbarPanel name="User Info" searchBar={false} path={ROUTES.HOME} />
-
-         <div className="ro">
+         <NavbarPanel name="Company Info" searchBar={false} path={ROUTES.HOME} />
+         {loading ? (
+            <div className="mt-5">
+               <LoaderAnimation />
+            </div>
+         ) : (
             <div className="col-12 col-sm-10 col-md-10 col-lg-6 offset-sm-1 offset-md-1 offset-lg-3 mb-5 py-3">
                <div className="card mb-3 shadow">
                   <div className="card-body">
@@ -30,8 +36,8 @@ const CompanyInfo = () => {
                   </div>
                )}
             </div>
-            <Footer />
-         </div>
+         )}
+         <Footer />
       </div>
    );
 };
