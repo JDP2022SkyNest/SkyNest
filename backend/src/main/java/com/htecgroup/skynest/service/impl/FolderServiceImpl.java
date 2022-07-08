b6756e53727482c3f1cbe6c7e444c8af5fd52405
MarkoRadunovicHtec <105654724+MarkoRadunovicHtec@users.noreply.github.com>
2022-07-08 12:31:03 +0200

    SJQ2T-198 Endpoint for view bucket content (#156)
    * FolderFile response class added, method for return folders and files from bucket in bucket service, corresponding methods added in repo classes
    
    * Endpoint for getBucketContent added in bucketController, methods refactored to work properly
    
    * Extracting methods for getAll Folder and Files from root into corresponding services, FolderFileResponse class renamed to StorageContentResponse
    
    * Methods renamed to be more meaningful
    
    * getAllFolderFromParent method name renamed to getAllFolderWithParent, same in the File service for files
    
    * Circular dependencies fix after merge with dev, BucketRepository injected in FolderServiceImpl instead of BucketService, tests fixed
    
    * Tests added for File and Folder services for corresponding methods
    
    * uuid changed to bucketId
    
    * Swagger documentation fix
    
    * Methods renamed properly
    
    * Little swagger fix

--- backend/src/main/java/com/htecgroup/skynest/service/impl/FolderServiceImpl.java
+++ backend/src/main/java/com/htecgroup/skynest/service/impl/FolderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.htecgroup.skynest.service.impl;
 
 import com.htecgroup.skynest.annotation.ParentFolderIsInTheSameBucket;
+import com.htecgroup.skynest.exception.buckets.BucketNotFoundException;
 import com.htecgroup.skynest.exception.folder.FolderNotFoundException;
 import com.htecgroup.skynest.model.dto.LoggedUserDto;
 import com.htecgroup.skynest.model.entity.BucketEntity;
@@ -8,9 +9,9 @@ import com.htecgroup.skynest.model.entity.FolderEntity;
 import com.htecgroup.skynest.model.entity.UserEntity;
 import com.htecgroup.skynest.model.request.FolderCreateRequest;
 import com.htecgroup.skynest.model.response.FolderResponse;
+import com.htecgroup.skynest.repository.BucketRepository;
 import com.htecgroup.skynest.repository.FolderRepository;
 import com.htecgroup.skynest.repository.UserRepository;
-import com.htecgroup.skynest.service.BucketService;
 import com.htecgroup.skynest.service.CurrentUserService;
 import com.htecgroup.skynest.service.FolderService;
 import lombok.AllArgsConstructor;
@@ -19,7 +20,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -32,7 +35,7 @@ public class FolderServiceImpl implements FolderService {
 
   private UserRepository userRepository;
 
-  private BucketService bucketService;
+  private BucketRepository bucketRepository;
 
   @Override
   public FolderResponse createFolder(
@@ -57,7 +60,8 @@ public class FolderServiceImpl implements FolderService {
       UUID parentFolderEntityId) {
     UserEntity currentUser = userRepository.getById(currentUserId);
     BucketEntity bucketEntity =
-        modelMapper.map(bucketService.findBucketById(bucketEntityId), BucketEntity.class);
+        bucketRepository.findById(bucketEntityId).orElseThrow(BucketNotFoundException::new);
+    bucketEntity = modelMapper.map(bucketEntity, BucketEntity.class);
     FolderEntity parentFolderEntity = folderRepository.findFolderById(parentFolderEntityId);
 
     folderEntity.setParentFolder(parentFolderEntity);
@@ -73,5 +77,24 @@ public class FolderServiceImpl implements FolderService {
         folderRepository.findById(uuid).orElseThrow(FolderNotFoundException::new);
     FolderResponse folderResponse = modelMapper.map(folderEntity, FolderResponse.class);
     return folderResponse;
+  }
+
+  @Override
+  public List<FolderResponse> getAllRootFolders(UUID bucketId) {
+    List<FolderEntity> allFolders =
+        folderRepository.findAllByBucketIdAndParentFolderIsNull(bucketId);
+    return asFolderResponseList(allFolders);
+  }
+
+  @Override
+  public List<FolderResponse> getAllFoldersWithParent(UUID parentFolderId) {
+    List<FolderEntity> allFolders = folderRepository.findAllByParentFolderId(parentFolderId);
+    return asFolderResponseList(allFolders);
+  }
+
+  private List<FolderResponse> asFolderResponseList(List<FolderEntity> allFolders) {
+    return allFolders.stream()
+        .map(folder -> modelMapper.map(folder, FolderResponse.class))
+        .collect(Collectors.toList());
   }
 }
