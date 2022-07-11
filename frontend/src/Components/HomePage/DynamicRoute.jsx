12692a1ac010f725899efa1eb61842f00d22c7bb
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-07-11 13:55:15 +0200

    Dynamic Styling + Overall Styling

--- frontend/src/Components/HomePage/DynamicRoute.jsx
+++ frontend/src/Components/HomePage/DynamicRoute.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { bucketContent } from "../ReusableComponents/ReusableFunctions";
+import NavbarPanel from "../ReusableComponents/NavbarPanel";
+import ROUTES from "../Routes/ROUTES";
+import Footer from "../Footer/Footer";
 
 const DynamicRoute = () => {
    const { routeId } = useParams();
@@ -10,7 +13,12 @@ const DynamicRoute = () => {
       bucketContent(accessToken, routeId);
    }, [routeId, accessToken]);
 
-   return <div>DynamicRoute</div>;
+   return (
+      <div className="dynamic-route-body">
+         <NavbarPanel name="User Info" searchBar={false} path={ROUTES.HOME} />
+         <Footer />
+      </div>
+   );
 };
 
 export default DynamicRoute;
