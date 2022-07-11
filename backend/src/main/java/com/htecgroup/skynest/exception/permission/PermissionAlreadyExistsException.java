5ef4c23fda8abbc1f6e84365431b4b39099ffcf3
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-07-11 11:48:57 +0200

    docs and spelling

--- backend/src/main/java/com/htecgroup/skynest/exception/permission/PermissionAlreadyExistsException.java
+++ backend/src/main/java/com/htecgroup/skynest/exception/permission/PermissionAlreadyExistsException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 public class PermissionAlreadyExistsException extends ActionException {
 
   private static final long serialVersionUID = -2005556949503604098L;
-  private static final String MESSAGE = "User already has some kind of access for requested object";
+  private static final String MESSAGE = "User already has some kind of access to requested object";
 
   public PermissionAlreadyExistsException() {
     super(MESSAGE, HttpStatus.CONFLICT);
