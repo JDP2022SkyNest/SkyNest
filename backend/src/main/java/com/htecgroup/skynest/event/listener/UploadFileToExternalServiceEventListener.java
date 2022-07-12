fca39e0b621afb51adfb23347e3d66272c4255ce
MetoMladenovski <metodi.mladenovski@htecgroup.com>
2022-07-12 17:15:52 +0200

    added comment for publishing events

--- backend/src/main/java/com/htecgroup/skynest/event/listener/UploadFileToExternalServiceEventListener.java
+++ backend/src/main/java/com/htecgroup/skynest/event/listener/UploadFileToExternalServiceEventListener.java
@@ -9,7 +9,8 @@ public class UploadFileToExternalServiceEventListener
     implements ApplicationListener<UploadFileToExternalServiceEvent> {
 
   // TODO: Inject factory for creating lambdas. Create lambda for event and execute it.
-
+  // Publishing events is going to be done in the service on the action that we want our lambda to
+  // be executed on.
   @Override
   public void onApplicationEvent(UploadFileToExternalServiceEvent event) {}
 }
