c12e6293218cf7b8f3255aeac512e3a1072585c1
Miljan Pantovic <miljan.pantovic@htecgroup.com>
2022-07-14 10:06:57 +0200

    Additional styling

--- frontend/src/Components/HomePage/Files/Files.jsx
+++ frontend/src/Components/HomePage/Files/Files.jsx
@@ -15,7 +15,7 @@ const Files = ({ elem, setErrorMsg, setSuccessMsg }) => {
       <div className="col-6 col-sm-6 col-md-3 col-lg-2 p-1 mt-2">
          <div className="cursor-pointer bg-white rounded">
             <div className="p-2 px-3">
-               <div className="text-overflow">
+               <div className="text-overflow file-text-width">
                   <AiCions.AiOutlineFile className="main-icon-align mr-1" />
                   {elem.name}
                </div>
