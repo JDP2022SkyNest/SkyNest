9a728b386eea4ffaa34c786cdf28ad52e80b7fbd
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-07-15 13:06:01 +0200

    folder contents now also includes path

--- backend/src/main/java/com/htecgroup/skynest/model/response/ShortFolderResponse.java
+++ backend/src/main/java/com/htecgroup/skynest/model/response/ShortFolderResponse.java
@@ -3,17 +3,15 @@ package com.htecgroup.skynest.model.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-public class StorageContentResponse {
-
-  private UUID bucketId;
-  private List<FolderResponse> folders;
-  private List<FileResponse> files;
-  private List<ShortFolderResponse> path;
+public class ShortFolderResponse {
+  private UUID id;
+  private String name;
 }
