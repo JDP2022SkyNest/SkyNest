30d9a31edaca797b06711818f18801f2f4d4edee
Ivan Fajgelj <ivan.fajgelj@htecgroup.com>
2022-07-15 17:35:29 +0200

    added serialVersionUID to exception

--- backend/src/main/java/com/htecgroup/skynest/exception/folder/FolderParentIsDeletedException.java
+++ backend/src/main/java/com/htecgroup/skynest/exception/folder/FolderParentIsDeletedException.java
@@ -3,6 +3,8 @@ package com.htecgroup.skynest.exception.folder;
 import org.springframework.http.HttpStatus;
 
 public class FolderParentIsDeletedException extends FolderException {
+
+  private static final long serialVersionUID = -5745928446505974461L;
   public static final String MESSAGE = "Folder's parent is deleted";
 
   public FolderParentIsDeletedException() {
