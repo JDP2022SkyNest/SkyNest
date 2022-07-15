35b09b763022fbc523ea8e227b5ae578c1151463
MetoMladenovski <metodi.mladenovski@htecgroup.com>
2022-07-15 10:03:26 +0200

    fixed some tests.

--- backend/src/test/java/com/htecgroup/skynest/service/impl/FolderServiceImplTest.java
+++ backend/src/test/java/com/htecgroup/skynest/service/impl/FolderServiceImplTest.java
@@ -14,6 +14,7 @@ import com.htecgroup.skynest.repository.UserRepository;
 import com.htecgroup.skynest.service.ActionService;
 import com.htecgroup.skynest.service.CurrentUserService;
 import com.htecgroup.skynest.service.FileService;
+import com.htecgroup.skynest.service.PermissionService;
 import com.htecgroup.skynest.utils.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -36,6 +37,7 @@ class FolderServiceImplTest {
   @Mock private CurrentUserService currentUserService;
 
   @Mock private ActionService actionService;
+  @Mock private PermissionService permissionService;
   @Mock private UserRepository userRepository;
   @Spy private ModelMapper modelMapper;
   @Mock private FileService fileService;
