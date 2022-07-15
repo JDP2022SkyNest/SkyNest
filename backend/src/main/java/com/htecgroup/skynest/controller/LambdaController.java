8c6774906bdd6ddb676c9094c1639dd1bd7271b4
MetoMladenovski <metodi.mladenovski@htecgroup.com>
2022-07-15 11:34:05 +0200

    added swagger

--- backend/src/main/java/com/htecgroup/skynest/controller/LambdaController.java
+++ backend/src/main/java/com/htecgroup/skynest/controller/LambdaController.java
@@ -1,7 +1,14 @@
 package com.htecgroup.skynest.controller;
 
 import com.htecgroup.skynest.lambda.LambdaType;
+import com.htecgroup.skynest.model.response.ErrorMessage;
 import com.htecgroup.skynest.service.BucketService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -29,10 +36,43 @@ public class LambdaController {
     return Arrays.stream(LambdaType.values()).collect(Collectors.toList());
   }
 
+  @Operation(summary = "Get all active lambdas for bucket")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "All active lambdas returned",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = LambdaType.class),
+                  examples = {
+                    @ExampleObject(
+                        value =
+                            "[\"UPLOAD_FILE_TO_EXTERNAL_SERVICE_LAMBDA\","
+                                + "\"SOME_OTHER_LAMBDA\"]")
+                  })
+            }),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Unauthorized request",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorMessage.class),
+                  examples = {
+                    @ExampleObject(
+                        value =
+                            "{\"messages\":[\"Access denied\"],"
+                                + " \"status\": \"401\","
+                                + " \"timestamp\": \"2022-06-07 16:18:12\"}")
+                  })
+            }),
+      })
   @GetMapping("/active/bucket/{bucketId}")
   public List<LambdaType> getActiveLambdasForBucket(@PathVariable UUID bucketId) {
     List<LambdaType> activeLambdas = bucketService.getActiveLambdas(bucketId);
-    log.info("Successfully got active lambdas for bucket {}", bucketId);
+    log.info("Successfully got {} active lambdas for bucket {}", activeLambdas, bucketId);
     return activeLambdas;
   }
 }
