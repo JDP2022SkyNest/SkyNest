65c43b09ed169169865bf6a20e69f47cdc659849
Marko Radunovic <marko.radunovic@htecgroup.com>
2022-07-19 15:45:36 +0200

    Flyway script initial setup

---
+++ backend/src/main/resources/db/migration/V14.2__object_to_tag_create.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `object_to_tag` (
+  `tag_id` binary(16) NOT NULL,
+  `object_id` binary(16) NOT NULL,
+  PRIMARY KEY (`tag_id`,`object_id`),
+  CONSTRAINT `fk_object_to_tag_tag_id`
+      FOREIGN KEY (`tag_id`)
+      REFERENCES `tag` (`id`)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_object_access_object_id`
+      FOREIGN KEY (`object_id`)
+      REFERENCES `object` (`id`)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
