c2078b966b3cf519e6aba316771f83c9d731d41f
DarkoVeljanoskiHtec <105631484+DarkoVeljanoskiHtec@users.noreply.github.com>
2022-07-20 15:53:59 +0200

    SJQ2T-444 BE Inappropriate Message For Moving File Bug Fix (#233)
    * added is file deleted checks
    
    * small change

--- backend/src/main/java/com/htecgroup/skynest/service/impl/FileServiceImpl.java
+++ backend/src/main/java/com/htecgroup/skynest/service/impl/FileServiceImpl.java
@@ -174,6 +174,7 @@ public class FileServiceImpl implements FileService {
   @Override
   public void moveFileToFolder(UUID fileId, UUID destinationFolderId) {
     FileMetadataEntity fileMetadataEntity = findFileMetaDataEntity(fileId);
+    checkIfDeleted(fileMetadataEntity);
     FolderEntity folderEntity =
         folderRepository.findById(destinationFolderId).orElseThrow(FolderNotFoundException::new);
     checkIfFileAlreadyInsideFolder(fileMetadataEntity, folderEntity);
@@ -184,9 +185,16 @@ public class FileServiceImpl implements FileService {
   @Override
   public void moveFileToRoot(UUID fileId) {
     FileMetadataEntity fileMetadataEntity = findFileMetaDataEntity(fileId);
+    checkIfDeleted(fileMetadataEntity);
     checkIfFileIsAlreadyInsideRoot(fileMetadataEntity);
     fileMetadataEntity.moveToRoot(fileMetadataEntity);
     saveMoveFile(fileMetadataEntity);
+  }
+
+  private void checkIfDeleted(FileMetadataEntity fileMetadataEntity) {
+    if (fileMetadataEntity.isDeleted()) {
+      throw new FileAlreadyDeletedException();
+    }
   }
 
   private void checkIfFileAlreadyInsideFolder(
