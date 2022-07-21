2e1a12e1ee793d9d3e296386c0c77005d90a2186
Darko Veljanovski <darko.veljanoski@htecgroup.com>
2022-07-21 13:57:17 +0200

    check permission for getBucketContent bucket

--- backend/src/main/java/com/htecgroup/skynest/service/impl/BucketServiceImpl.java
+++ backend/src/main/java/com/htecgroup/skynest/service/impl/BucketServiceImpl.java
@@ -7,10 +7,7 @@ import com.htecgroup.skynest.exception.buckets.BucketNotFoundException;
 import com.htecgroup.skynest.lambda.LambdaType;
 import com.htecgroup.skynest.model.dto.BucketDto;
 import com.htecgroup.skynest.model.dto.LoggedUserDto;
-import com.htecgroup.skynest.model.entity.ActionType;
-import com.htecgroup.skynest.model.entity.BucketEntity;
-import com.htecgroup.skynest.model.entity.CompanyEntity;
-import com.htecgroup.skynest.model.entity.UserEntity;
+import com.htecgroup.skynest.model.entity.*;
 import com.htecgroup.skynest.model.request.BucketCreateRequest;
 import com.htecgroup.skynest.model.request.BucketEditRequest;
 import com.htecgroup.skynest.model.response.BucketResponse;
@@ -168,6 +165,8 @@ public class BucketServiceImpl implements BucketService {
 
   @Override
   public StorageContentResponse getBucketContent(UUID bucketId) {
+
+    permissionService.currentUserHasPermissionForBucket(bucketId, AccessType.VIEW);
 
     if (!bucketRepository.existsById(bucketId)) throw new BucketNotFoundException();
 
