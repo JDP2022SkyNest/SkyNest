048cd948ad6937f543b8f4ade6bbc192e78a8c76
Miljan Pantovic <105703170+casualme2@users.noreply.github.com>
2022-07-26 14:55:36 +0200

    FE_QoL_And_Refactoring_Part_2 (#281)
    * Loader Allign
    
    * Logout is now faster (does not await the response)
    
    * Upload Error Msg

--- frontend/src/Components/Loader/LoaderAnimation.jsx
+++ frontend/src/Components/Loader/LoaderAnimation.jsx
@@ -4,7 +4,7 @@ import "./Loader.css";
 const LoaderAnimation = () => (
    <div
       className="text-center loader-color mt-5"
-      style={{ position: "absolute", left: "50%", top: "40%", transform: "translate(-50%,-50%)", zIndex: "100" }}
+      style={{ position: "absolute", left: "50%", top: "43%", transform: "translate(-50%,-50%)", zIndex: "100" }}
    >
       <div className="spinner-border loader-circle-size" role="status"></div>
       <br />
