251691406914cad62c2543323891fa76eccb34b4
Miljan Pantovic <105703170+casualme2@users.noreply.github.com>
2022-07-26 13:38:56 +0200

    FE_QoL_And_Refactoring (#274)
    * TagRemove Modal closes on final Tag removed
    
    * Remove Tag Btn not available if there're no tags
    
    * Loader is not in the Middle
    
    * unTag modal Loader

--- frontend/src/Components/Loader/LoaderAnimation.jsx
+++ frontend/src/Components/Loader/LoaderAnimation.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./Loader.css";
 
 const LoaderAnimation = () => (
-   <div className="text-center loader-color mt-5" style={{ position: "absolute", left: "50%", transform: "translate(-50%)", zIndex: "100" }}>
+   <div
+      className="text-center loader-color mt-5"
+      style={{ position: "absolute", left: "50%", top: "40%", transform: "translate(-50%,-50%)", zIndex: "100" }}
+   >
       <div className="spinner-border loader-circle-size" role="status"></div>
       <br />
       {/* <div className="text-muted loader-text-size">Loading your data...</div> */}
