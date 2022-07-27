063ebbdec1cc5ed124fd850f280eb8f38417338a
Dragan Mitrašinović <105700040+DraganMitrasinovicHtec@users.noreply.github.com>
2022-07-27 10:45:36 +0200

    Using email for grant and edit permission requests (#290)

--- backend/src/main/java/com/htecgroup/skynest/model/request/PermissionEditRequest.java
+++ backend/src/main/java/com/htecgroup/skynest/model/request/PermissionEditRequest.java
@@ -1,20 +1,23 @@
 package com.htecgroup.skynest.model.request;
 
+import com.htecgroup.skynest.util.RegexUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.UUID;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PermissionEditRequest {
 
-  @NotNull(message = "user id cannot be null or empty")
-  private UUID grantedTo;
+  @NotNull(message = "cannot be null")
+  @Pattern(message = "format is not valid", regexp = RegexUtil.EMAIL_FORMAT_REGEX)
+  @Size(max = 254, message = "length cannot be over 254 characters")
+  private String grantedToEmail;
 
   @Pattern(regexp = "(view)|(download)|(edit)|(owner)")
   private String access;
